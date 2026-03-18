0a98aafda92af61068562bec52863b3b235657c8
Simon Praetorius <moin@praetorius.me>
2026-03-18 13:49:15 +0100

    [TASK] Update dependencies, actions, node and publishing workflow
    Since the frontend building setup broke (again), I made the decision
    to drop support for CommonJS, which should no longer be necessary
    anyways and already lead to warnings by vite in the past. This
    simplifies the setup considerably and allows to update dependencies:
    
    * new GitHub Actions (tagged to commit hashes)
    * node 24 in GitHub Actions
    * updated TypeScript
    * Token-less npm publishing
    * use vite 8 in testing setup

--- tests/distribution/javascript-cjs/vite.config.js
+++
@@ -1,6 +0,0 @@
-const defineConfig = require("vite").defineConfig;
-const autoOrigin = require("vite-plugin-auto-origin").default;
-
-module.exports = defineConfig({
-    plugins: [autoOrigin()],
-});
